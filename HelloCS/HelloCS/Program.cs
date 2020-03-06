6b18563362b0e224cc44ace7ef1e1010cdd05222
Bohdan Shkamarida <s18881@pjwstk.edu.pl>
2020-03-06 09:29:59 +0100

    initial commit

---
+++ HelloCS/HelloCS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HelloCS
+{
+    public class Program{
+        public static async Task Main(string[] args){
+            var link = args[0];
+            var httpClient = new HttpClient();
+            var response =  await httpClient.GetAsync(link);
+            if (response.IsSuccessStatusCode) {
+                var content = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]", RegexOptions.IgnoreCase);
+                var emails = regex.Matches(content);
+                foreach (var item in emails){
+                    Console.WriteLine("Email: {0}", item);
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
